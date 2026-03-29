9310503df203dc41814f0f5f9341469fca172e43
chriscorbell <88693878+chriscorbell@users.noreply.github.com>
2026-03-29 18:09:07 -0400

    .

--- hosts/devbox/hardware-configuration.nix
+++
@@ -1,5 +0,0 @@
-{ ... }:
-{
-  # Replace this placeholder with the generated hardware configuration:
-  # sudo nixos-generate-config --show-hardware-config > ~/.config/nix/hosts/devbox/hardware-configuration.nix
-}
