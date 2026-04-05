2fa02d981ca1cb5cd9fb05b9290519388289aee1
chriscorbell <88693878+chriscorbell@users.noreply.github.com>
2026-04-05 16:30:20 -0400

    .

--- modules/nixos/common/users.nix
+++ modules/nixos/common/users.nix
@@ -4,6 +4,7 @@
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = [ "wheel" ];
+    hashedPassword = "$y$j9T$EUZq8mja/o9MlpBWiPK0e/$dRiAy7yapAfU8ZhPRzjZgC/p0Z5TaFflSI6UUz8IK72";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDn5vaXh3vNvgZLDM4QhAqwLvUy/fwUpnuDPnWNrnKf1 hi@chriscorbell.com"
     ];
